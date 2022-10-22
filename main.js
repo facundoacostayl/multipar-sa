d0b08043d045236bd0caa14989396cbfaa02ffef
Facundo Acosta <facundoacostayl@outlook.com>
2022-10-22 17:16:36 -0300

    cursor effect on all sections

--- main.js
+++ main.js
@@ -39,8 +39,10 @@ window.addEventListener("scroll", () => {
 
 // CURSOR EFFECT
 
-const cursor = document.querySelector('.cursor');
+const cursors = document.querySelectorAll('.cursor');
 document.addEventListener('mousemove', (e) => {
-    cursor.style.left = e.clientX + 'px';
-    cursor.style.top = e.clientY + 'px';
+    cursors.forEach(cursor => {
+        cursor.style.left = e.clientX + 'px';
+        cursor.style.top = e.clientY + 'px';
+    })
 })
