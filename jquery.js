1ed2434f706aa2888ce36d6fc4f538cc5546ca6c
Facundo Acosta <facundoacostayl@outlook.com>
2022-11-09 07:50:17 -0300

    popup responsive updates

--- jquery.js
+++ jquery.js
@@ -13,6 +13,8 @@ $(window).scroll(function () {
 
 // TITLE PARALLAX EFFECT ON (CONTACT: Y Position 3492) (SERVICIOS TOP -20%)
 
+const standardMobile = window.matchMedia("(min-height: 736px)");
+const bigStandardMobile = window.matchMedia("(min-height: 850px)");
 const mediumWindow = window.matchMedia("(min-width: 540px)");
 const desktopWindow = window.matchMedia("(min-width: 768px)");
 const contactoSection = document.querySelector("#contacto-section");
@@ -20,14 +22,57 @@ const contactoSection = document.querySelector("#contacto-section");
 
   $(document).on("scroll", function () {
     console.log(window.scrollY)
-    console.log("MOBILE")
-  if (window.scrollY < 2970) {
+  if (window.scrollY < 3061) {
     $(".contacto-title").css(
       "top",
-      Math.max(100 - 0.35 * (window.scrollY - 2790)) + "px"
+      Math.max(100 - 0.35 * (window.scrollY - 2881)) + "px"
     );
   }
 });
+
+$(document).on("scroll", function () {
+  if (window.scrollY < 1350) {
+    $(".servicios-title").css(
+      "top",
+      Math.max(100 - 0.35 * (window.scrollY - 865)) + "px"
+    );
+  }
+});
+
+
+
+if(standardMobile.matches) {
+  $(document).on("scroll", function () {
+    console.log(window.scrollY)
+  if (window.scrollY < 3600) {
+    $(".contacto-title").css(
+      "top",
+      Math.max(220 - 0.35 * (window.scrollY - 3100)) + "px"
+    );
+  }
+});
+}
+
+if(bigStandardMobile.matches) {
+  if (window.scrollY < 4067) {
+    $(".contacto-title").css(
+      "top",
+      Math.max(220 - 0.35 * (window.scrollY - 3674)) + "px"
+    );
+  }
+}
+
+if(mediumWindow.matches) {
+  $(document).on("scroll", function () {
+    console.log(window.scrollY)
+  if (window.scrollY < 3473) {
+    $(".contacto-title").css(
+      "top",
+      Math.max(220 - 0.35 * (window.scrollY - 2973)) + "px"
+    );
+  }
+});
+
 
 $(document).on("scroll", function () {
   if (window.scrollY < 1525) {
@@ -38,15 +83,11 @@ $(document).on("scroll", function () {
   }
 });
 
-
-//FOLLOW HERE
-if(desktopWindow.matches) {
 }
 
 if(desktopWindow.matches) {
   $(document).on("scroll", function () {
     console.log(window.scrollY)
-    console.log("DESKTOP")
   if (window.scrollY < 4170) {
     $(".contacto-title").css(
       "top",
@@ -54,14 +95,4 @@ if(desktopWindow.matches) {
     );
   }
 });
-
-$(document).on("scroll", function () {
-  if (window.scrollY < 1525) {
-    $(".servicios-title").css(
-      "top",
-      Math.max(100 - 0.35 * (window.scrollY - 1175)) + "px"
-    );
-  }
-});
 }
-
